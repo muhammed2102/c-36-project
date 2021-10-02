7b95756f24d1e0b32052521da930199fa11a5bf7
muhammed sinan.p <psinan32@gmail.com>
2021-10-02 10:26:39 +0530

    add platform

--- js/Question.js
+++ js/Question.js
@@ -25,9 +25,9 @@ class Question {
     this.title.html("MyQuiz Game");
     this.title.position(350, 0);
 
-    // this.question("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    //this.question("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
     // this.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    //this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
     // question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
 
     this.question.position(150, 80);
@@ -46,8 +46,8 @@ class Question {
 
     this.button.mousePressed(()=>{
       
-      // this.message.html("Thank You, Your Answer Has Been Submitted");
-      // this.message.position(350, 350);
+       this.message.html("Thank You, Your Answer Has Been Submitted");
+       this.message.position(350, 350);
 
       // this.message.html("Thank You, Your Answer Has Been Submitted");
       // this.message.position();
